33d656c98a8c797e59934163224632bfb9e330b8
jaydeb-ds <das294761@gmail.com>
2026-03-20 21:44:57 +0530

    day7

---
+++ day7/shop.java
@@ -0,0 +1,66 @@
+class mrp{
+		double mrp;
+
+		void getdata(int x)
+		{
+			mrp = x;
+		}
+
+		void display()
+		{
+			System.out.println("MRP = "+mrp);
+		}
+	}
+
+class bill extends mrp
+{
+	double gst = 0, delivery = 0, discount = 0;
+	double total;
+
+	void getGst(double x)
+	{
+		gst = (mrp * x) /100;
+	}
+
+	void getdDelivery(int x)
+	{
+		delivery = x;
+	}
+
+	void getDiscount(int x)
+	{
+		discount = (mrp * x) / 100;
+	}
+
+	void cal()
+	{
+		total = (mrp + delivery + gst) - discount;
+	}
+
+	void display()
+	{
+		System.out.println("MRP = "+mrp);
+		System.out.println("GST = "+gst);
+		System.out.println("Delivery Charges = "+delivery);
+		System.out.println("Discount = "+discount);
+		System.out.println("Total Bill = "+total);
+	}
+
+}
+
+public class shop{
+			public static void main(String[] args) {
+				bill b1 = new bill();
+				b1.getdata(2345);
+				b1.getGst(18);
+				b1.getdDelivery(50);
+				b1.getDiscount(20);
+				b1.cal();
+				b1.display();
+			}
+		}
+
+
+
+
+
