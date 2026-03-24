9a91c7e440a71ac95f50a6d360e09a11e949da01
jaydeb-ds <das294761@gmail.com>
2026-03-24 19:55:16 +0530

    string methods

---
+++ day11/string.java
@@ -0,0 +1,24 @@
+public class string{
+		public static void main(String[] args)
+		{
+			String s1 = "Jaydeb";
+			String s2 = " Das";
+			String s3 = "Jaydeb";
+
+			System.out.println(s1.charAt(2));
+
+			System.out.println(s1.equals(s3));
+
+			System.out.println(s1.concat(s2));
+
+			System.out.println(s1.toLowerCase());
+
+			System.out.println(s1.toUpperCase());
+
+			System.out.println(s1.indexOf('y'));
+
+			System.out.println(s1.substring(0,3));
+
+			System.out.println(s1.length());
+		}
+	}
