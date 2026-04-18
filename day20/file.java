55941b57941c691837366fa0f54bf36c64dc6034
jaydeb-ds <das294761@gmail.com>
2026-04-18 21:28:37 +0530

    day 20 1st

---
+++ day20/file.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class file
+{
+	public static void main(String[] args)
+	{
+		File f = new File("test.txt");
+
+		File dir = new File("test");
+
+		System.out.println("Exists: "+f.exists());
+
+		System.out.println("Readable: "+f.canRead());
+
+		System.out.println("Writable: "+f.canWrite());
+
+		System.out.println("is File: "+f.isFile());
+
+		System.out.println("is Directory "+f.isDirectory());
+
+		System.out.println("is F1 Directory: "+dir.isDirectory());
+
+		System.out.println("File Size: "+f.length());
+
+
+		String[] files = dir.list();
+
+		 System.out.println("Files in directory:");
+		 for(String name : files) {
+			 System.out.println(name);
+        }
+
+	}
+}
