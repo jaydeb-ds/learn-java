664d91c12a7c54fff674ff13e7f74e321d007c92
jaydeb-ds <das294761@gmail.com>
2026-04-29 21:40:21 +0530

    24 days

---
+++ day16/name2.java
@@ -0,0 +1,16 @@
+public class name2{
+   public static void main(String[] args)
+   {
+        String name = "Jay Deb Das";
+        String arr[] = name.split(" ");
+        String newName = "";
+        for(int i = 0; i<arr.length-1; i++)
+        {
+            newName = newName + arr[i].charAt(0) + ". ";
+        }
+        newName = newName + arr[arr.length-1];
+        System.out.println("new name is:");
+        System.out.println(newName);
+
+   }   
+}
